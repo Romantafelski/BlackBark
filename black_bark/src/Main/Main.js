b781bd67cdf8c19ad6b1e2735993dc8ae8e2f452
Roman Tafelski <romantafelski@gmail.com>
2024-06-27 19:47:13 -0700

    updating

--- black_bark/src/Main/Main.js
+++ black_bark/src/Main/Main.js
@@ -2,16 +2,32 @@ import React from "react";
 import "./Main.css";
 import Facebook from "../Images/Facebook.png";
 import Insta from "../Images/Insta.png";
-import Logo from "../Images/Logo.jpg"
+import Logo from "../Images/Logo.png";
+import Background from "../Images/Background.jpg"
 import { Link } from "react-router-dom";
 
 function Main() {
   return (
-    <div>
-      <span className="blackBark">
-      <h1 className="bbText">Black Bark BBQ</h1>
-      <img className="logo" src={Logo} />
+    <div className="Main">
+      <div className="nameSpan">
+        <h1 className="blackBark">Black Bark BBQ</h1>
+        <img className="logo" src={Logo} />
+      </div>
+      <span className="address">
+        <p className="add">1234 Criss Crossing</p>
+        <p className="add">Chandler, AZ</p>
+        <p className="add">85118</p>
       </span>
+      <span className="hours">
+        <p className="day">Monday-Friday 11am-9pm</p>
+        <p className="day">Saturday 11am-10pm</p>
+        <p className="day">Sunday 11am-9pm</p>
+      </span>
+      <div className="socialImages">
+        <h4 className="followUs">Follow Us</h4>
+        <img className="fb" src={Facebook} />
+        <img className="insta" src={Insta} />
+      </div>
       <span className="heading">
         <Link to="/">
           <button className="item" id="home">
@@ -29,23 +45,6 @@ function Main() {
           </button>
         </Link>
       </span>
-      <span className="address">
-        <p className="add">1234 Criss Crossing</p>
-        <p className="add">Chandler, AZ</p>
-        <p className="add">85118</p>
-      </span>
-      <span className="hours">
-        <p className="day">Monday-Friday 11am-9pm</p>
-        <p className="day">Saturday 11am-10pm</p>
-        <p className="day">Sunday 11am-9pm</p>
-      </span>
-      <div className="socials">
-        <h4 className="followUs">Follow Us</h4>
-      <span className="socialImages">
-        <img className="fb" src={Facebook} />
-        <img className="insta" src={Insta} />
-      </span>
-      </div>
     </div>
   );
 }

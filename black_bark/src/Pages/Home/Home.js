1a7b2c88006f78ce2ff0051d17db4dfb29274287
Roman Tafelski <romantafelski@gmail.com>
2024-07-12 01:14:39 -0700

    changed layout and logo

--- black_bark/src/Pages/Home/Home.js
+++ black_bark/src/Pages/Home/Home.js
@@ -2,18 +2,18 @@ import React from "react";
 import "./Home.css";
 import Main from "../../Components/Main/Main";
 import Heading from "../../Components/Heading/Heading";
-import Logo from "./Logo.png";
+import Logo from "../../Components/Images/Logo.jpg";
 import Background from "../../Components/Images/Background.jpg";
 
 function Home() {
    return (
       <div className="home">
+         <img className="background" src={Background} />
          <div className="info">
             <h1 className="blackBark">Black Bark BBQ</h1>
             <img src={Logo} className="logo" />
             <h2 className="soon">Coming soon to Gilbert, AZ</h2>
          </div>
-         <img className="background" src={Background} />
          <Main />
          <Heading />
       </div>

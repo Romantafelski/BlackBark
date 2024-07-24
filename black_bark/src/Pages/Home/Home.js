b7be0c963df0594347e103a64f3b5bd2566c6360
Roman Tafelski <romantafelski@gmail.com>
2024-07-23 21:09:53 -0700

    switching logos

--- black_bark/src/Pages/Home/Home.js
+++ black_bark/src/Pages/Home/Home.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./Home.css";
 import Main from "../../Components/Main/Main";
 import Heading from "../../Components/Heading/Heading";
-import Logo from "../../Components/Images/Logo.png";
+import BLogo from "../../Components/Images/BLogo.JPG";
 
 function Home() {
    return (
       <div className="home">
          <Main />
          <div className="info">
-            <img src={Logo} className="logo" />
+            <img src={BLogo} className="logo" />
             <h2 className="soon">Coming soon to Gilbert, AZ</h2>
          </div>
          <Heading />

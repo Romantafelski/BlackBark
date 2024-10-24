8bdff9061c1b60d01a3d159ea2784014fa8e1f1f
romantafelski <romantafelski@gmail.com>
2024-10-24 14:46:44 -0700

    ammending CSS

--- black_bark/src/Pages/Home/Home.js
+++ black_bark/src/Pages/Home/Home.js
@@ -1,25 +1,42 @@
 import React from "react";
 import "./Home.css";
-import Main from "../../Components/Main/Main";
 import Heading from "../../Components/Heading/Heading";
 import BLogo from "../../Components/Images/BLogo.png";
-
-
+import Background from "../../Components/Images/Background.jpg";
+import Insta from "../../Components/Images/Insta.png";
+import Facebook from "../../Components/Images/Facebook.png";
 
 
 function Home() {
    return (
-      <div className="home">
-         <Main />
-         <div className="info">
-         <h1 className="blackBark">Black Bark BBQ</h1>
-            <img src={BLogo} className="logo" />
-            <h3 className="est">Est. AZ 2024</h3>
-            <h2 className="soon">Coming soon!</h2>
+      <div>
+         <div className="home">
+            <img className="background" src={Background} />
+            <div className="info">
+               <h1 className="blackBark">Black Bark BBQ</h1>
+               <img src={BLogo} className="logo" />
+               <h3 className="est">Est. AZ 2024</h3>
+               <h2 className="soon">Coming soon!</h2>
+            </div>
+            <Heading />
+            <footer>
+               <div className="socials">
+                  <h2 className="label">Socials</h2>
+                  <span className="insta">
+                     <p className="soc" id="handle">
+                        <img src={Insta} id="socImage" /> @blackbarkllc
+                     </p>
+                  </span>
+                  <span className="facebook">
+                     <p className="soc" id="handle">
+                        <img src={Facebook} id="socImage" /> @Kingsofque
+                     </p>
+                  </span>
+               </div>
+            </footer>
          </div>
-         <Heading />
       </div>
-  );
+   );
 }
 
 export default Home;
